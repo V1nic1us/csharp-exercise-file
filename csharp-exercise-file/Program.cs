d148f5695a95834a275aba542d02d22abea97cfd
V1nic1us <marcusvinicius.mds16@gmail.com>
2023-11-22 23:31:56 -0400

    feat: passing product data to a csv file

--- csharp-exercise-file/Program.cs
+++ csharp-exercise-file/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+using csharp_exercise_file.Entities;
+
+namespace csharp_exercise_file
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            try
+            {
+                List<Product> products = [];
+                string caminhoDaPasta = @"C:\Users\viniv\source\repos\csharp-exercise-file\csharp-exercise-file\output";
+                string caminhoDoArquivo = @"C:\Users\viniv\source\repos\csharp-exercise-file\csharp-exercise-file\output\summary.csv";
+                using var sourceFile = new StreamReader(@"C:\Users\viniv\source\repos\csharp-exercise-file\csharp-exercise-file\Products.csv");
+                while (!sourceFile.EndOfStream)
+                {
+                    string line = sourceFile.ReadLine();
+                    string[] fields = line.Split(",");
+                    string name = fields[0];
+                    double price = double.Parse(fields[1]);
+                    products.Add(new Product(name, price));
+                }
+                if (Directory.Exists(caminhoDaPasta))
+                {
+                    using var outputFile = new StreamWriter(caminhoDoArquivo);
+                    foreach (Product product in products)
+                    {
+                        outputFile.WriteLine(product.Name + "," + product.Price);
+                    }
+                }
+                else
+                {
+                    Directory.CreateDirectory(caminhoDaPasta);
+                    using var outputFile = new StreamWriter(caminhoDoArquivo);
+                    foreach (Product product in products)
+                    {
+                        outputFile.WriteLine(product.Name + "," + product.Price);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
